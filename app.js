5dff721969218d98f6a5d6576187cccb49e61792
MJX.SH <70670860+Mrityunjay20@users.noreply.github.com>
2023-05-17 15:36:17 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+//jshint esversion:6
+
+const { json } = require('body-parser');
+const express = require('express');
+
+const app = express();
+
+app.get("/", (req, res) => {
+    var ipadress = req.ip;
+    var language = req.acceptsLanguages();
+    var software=req.get("User-Agent");
+     res.json({
+     ipadress: ipadress,
+     language:language[0],
+     software:software
+     });
+    });
+
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+  });
